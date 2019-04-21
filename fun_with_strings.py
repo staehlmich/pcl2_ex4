abe5929f6cf6afe748c05581f33968918badcfea
Michael <mycall.707@gmail.com>
2019-04-21 11:21:42 +0200

    initialize script

---
+++ fun_with_strings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf8
+# Author: Michael Staehli
+
+from typing import Iterable
+
+def longest_substrings(x: str, y: str) -> Iterable[str]:
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    pass
+
+def main():
+    print("hello there")
+
+if __name__ == '__main__':
+    main()
